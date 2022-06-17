708b91459c7172a63399a3c77102b9a80621e907
Arif Puji Setiawan <arif.p.setiawan.9@gmail.com>
2022-06-17 23:00:23 +0700

    Add task

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <h1>Call a Friend</h1>
+      <p>your friendly contact app</p>
+      <hr />
+    </>
+  );
+};
 
 export default Header;
